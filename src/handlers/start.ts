4b997f112f220144612377117923e9213af3d88e
amanrajput2001 <90300203+amanrajput2001@users.noreply.github.com>
2021-11-22 23:30:31 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -19,11 +19,11 @@ composer.command('start', (ctx) => {
       ctx.from!.first_name + ' ' + ctx.from!.last_name
     )}</a>\n` +
     `I Play Songs in Group Voice Chats.\n` +
-    `If you like this bot consider adding a star to the <a href="https://github.com/ArnabXD/TGVCBot">repository</a>`;
+    `If you like this bot consider adding a star to the <a href="https://github.com/amanrajput2001/Hqsupervcbot">repository</a>`;
   return ctx.reply(text, {
     reply_markup: new InlineKeyboard().url(
       'View Source',
-      'https://github.com/ArnabXD/TGVCBot'
+      'https://github.com/amanrajput2001/Hqsupervcbot'
     ),
     disable_web_page_preview: true,
     parse_mode: 'HTML'
